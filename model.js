496eecdaf43fac575f67a9f16da1c27af1f89687
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-09-30 09:17:12 +0200

    x

--- model.js
+++ model.js
@@ -3,6 +3,7 @@ const model = {
         loggedInUserID: null,
         pages: ["mainPage", "moviePage", "filterPage", "profilePage", "editProfile"],
         currentPage: "mainPage",
+        genres: ["Sci-Fi", "Action"]
     },
 
     input: {
@@ -23,12 +24,29 @@ const model = {
     },
 
     data: {
-        filmer: [
+        movies: [
             {
                 movieName: "Avatar",
-                genre: ["Sci-Fi", "Action"]
-            }
+                genre: [1 , 2],
+                rating: 930,
+                commnents: "Denne filmen var bra",
+                plot: `A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.`,
+                picture: "path",
+                trailer: "path",
+                length: 162,
+            },
+                
+        ],
 
+        profile: [
+            {
+                name: "Kaja",
+                email: "kaja@film.no",
+                tlf: null,
+                location: null,
+                bio: "Jeg liker filmer",
+                profilePicture: "path",
+            }
         ]
     },
 
