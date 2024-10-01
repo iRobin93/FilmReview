b0d900b87a68da95eefe164a2ea0394f31bbbe7b
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-10-01 12:16:59 +0200

    Update model.js

--- model.js
+++ model.js
@@ -3,7 +3,17 @@ const model = {
         loggedInUserID: null,
         pages: ["mainPage", "moviePage", "filterPage", "profilePage", "editProfile"],
         currentPage: "mainPage",
-        genres: ["Sci-Fi", "Action"]
+        filterBy: [
+            {
+                id: 1,
+                genre: "Sci-Fi",
+            },
+            {
+                id: 2,
+                genre: "Fantasy",
+            },
+
+        ]
     },
 
     input: {
@@ -24,30 +34,39 @@ const model = {
     },
 
     data: {
-        movies: [
+        filmer: 
+        [
             {
+                id: 1,
                 movieName: "Avatar",
-                genre: [1 , 2],
+                genre: [1, 2],
+                year: "2002",
+                length: 162,
+                comments: ["Denne filmen var veldig bra"],
                 rating: 930,
-                commnents: "Denne filmen var bra",
-                plot: `A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.`,
                 picture: "path",
                 trailer: "path",
-                length: 162,
-            },
-                
+                infoText: "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+
+            }
         ],
 
-        profile: [
-            {
-                name: "Kaja",
-                email: "kaja@film.no",
-                tlf: null,
-                location: null,
-                bio: "Jeg liker filmer",
-                profilePicture: "path",
-            }
-        ]
+        profile:
+        [
+           {
+            id: 1,
+            firstName: "Kaja",
+            lastName: "Kortfilm",
+            tlf: "",
+            bio: "Tekst her",
+            picture: "path",
+            email: "kaja@email.no",
+            place: "",
+            genres: ["Horror", "Fantasy", "Drama"],
+
+        } ,
+        
+        ] ,
     },
 
 }
